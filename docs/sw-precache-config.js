c0cac7a177d10e994eb790bd8d8201b1defc40dd
elifbon <elif@gdgistanbul.com>
2016-11-03 00:11:32 +0200

    supporters deploy

---
+++ docs/sw-precache-config.js
@@ -0,0 +1,1 @@
+module.exports={staticFileGlobs:["/index.html","/manifest.json","/bower_components/webcomponentsjs/webcomponents-lite.min.js","/src/**/*","/scripts/**/*","/images/**/*","/data/**/*"],navigateFallback:"/index.html"};
